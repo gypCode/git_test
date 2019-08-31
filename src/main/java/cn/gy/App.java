e1420fddfd610cc08295add6fbb4a2180c53fc6b
dfaf <fdafa>
2019-08-31 12:04:42 +0800

    测试提交

---
+++ src/main/java/cn/gy/App.java
@@ -0,0 +1,14 @@
+package cn.gy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int i=9;
+    }
+}
