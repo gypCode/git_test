fb770c13bd98cc9b643170c3f851aa825fe6d506
dfaf <fdafa>
2019-09-01 10:22:49 +0800

    lambda提交

---
+++ src/test/java/cn/gy/gitTest2.java
@@ -0,0 +1,21 @@
+package cn.gy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author gyp
+ * @date 2019/9/1 9:47
+ */
+public class gitTest2 {
+
+    public static void main(String[] args) {
+       System.out.println("我爱罗");
+        List<String> a=new ArrayList<>();
+        a.add("233");
+        a.add("80");
+        a.add("fsdfdf");
+        a.forEach(j->System.out.println("j"));
+
+    }
+}
